d07f70c8ce26769a5a9adcbb23744b81d797d66f
Victor Saraiva <victor_alex2005@hotmail.com>
2025-04-01 10:18:46 -0300

    feat: organização das pastas do backend

--- backend/app/utils/load_data.py
+++ backend/app/utils/load_data.py
@@ -2,7 +2,7 @@
 
 import mysql.connector
 
-from config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
+from app.config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
 from sql_util import ler_sql_arquivo
 
 
